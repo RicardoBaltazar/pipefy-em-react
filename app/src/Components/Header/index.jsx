ddf0f6060a807919d3fd7f1198f5689f1066920e
unknown <ricardo.baltazar04@gmail.com>
2021-03-10 10:42:58 -0300

    adicionndo componente Header

---
+++ app/src/Components/Header/index.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const HEADER = styled.header`
+    width: 100vw;
+    height: 4rem;
+    background-color: #009CA3;
+    box-shadow: 0px 4px 8px #00000029;
+`
+
+function Header(){
+    return (
+        <HEADER></HEADER>
+    )
+}
+
+export default Header;
