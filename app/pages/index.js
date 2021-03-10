efc7191ab53490245a3e361327c4df7494110b64
unknown <ricardo.baltazar04@gmail.com>
2021-03-10 11:01:32 -0300

    adicionando tag head para configurar as fontes do projeto

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,12 +1,19 @@
 import Head from 'next/head';
+import styled from 'styled-components';
 
-import Header from '../src/Components/Header'
+import Header from '../src/Components/Header';
+
+const Main = styled.main`
+  background-color: blue;
+`;
 
 export default function Home() {
   return (
     <div >
       <Header />
-      ola mundo
+      <Main>
+        ola mundo
+      </Main>
     </div>
   )
 }
--- app/src/Components/Header/index.jsx
+++ app/src/Components/Header/index.jsx
@@ -5,11 +5,19 @@ const HEADER = styled.header`
     height: 4rem;
     background-color: #009CA3;
     box-shadow: 0px 4px 8px #00000029;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 2rem;
+    
 `
 
 function Header(){
     return (
-        <HEADER></HEADER>
+        <HEADER>
+            Lista Rango
+        </HEADER>
     )
 }
 
